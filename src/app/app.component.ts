cf635eb3b37c0214d761dc859048373d20a0f0bc
sam-klok <sklok11@gmail.com>
2024-09-23 07:18:02 -0700

    adding dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-kendo-dd-template';
+  public listItems: Array<any> = [
+    {
+      text: 'Small',
+      value: 1,
+      url: 'https://www.telerik.com/kendo-angular-ui/components/dropdowns/dropdownlist',
+    },
+    {
+      text: 'Medium',
+      value: 2,
+      url: 'https://www.telerik.com/kendo-angular-ui/components/dropdowns/dropdownlist',
+    },
+    {
+      text: 'Large',
+      value: 3,
+      url: 'https://www.telerik.com/kendo-angular-ui/components/dropdowns/dropdownlist',
+    },
+  ];
+
+  public selectedValue = 2;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LabelModule } from "@progress/kendo-angular-label";
+import { InputsModule } from "@progress/kendo-angular-inputs";
+import { ButtonsModule } from "@progress/kendo-angular-buttons";
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputsModule,
+    BrowserAnimationsModule,
+    DropDownsModule,
+    LabelModule,
+    InputsModule,
+    ButtonsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
